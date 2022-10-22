f03e08d8b0086c04846906482ae7235f8ef6a620
Jaxe <42095078+Jaxe-Dev@users.noreply.github.com>
2022-10-22 14:37:02 +0800

    v7.0
    - Compiled for Rimworld 1.4

---
+++ source/Core/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace MoreRandomSeeds.Core
+{
+  internal static class Extensions
+  {
+    public static string Italic(this string self) => "<i>" + self + "</i>";
+    public static string Italic(this TaggedString self) => self.ToString().Italic();
+
+    public static string Bold(this string self) => "<b>" + self + "</b>";
+    public static string Bold(this TaggedString self) => self.ToString().Bold();
+  }
+}
